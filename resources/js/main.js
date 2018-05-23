4b9e91287e330c539667c53bb63b712a961ae7f9
Kevin Tate <2kmtate@gmail.com>
2018-05-23 18:37:01 -0400

    added carousel to jumbotron

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        draggable: false
+    });
+
+    
+});
+
+var userScroll = 0;
